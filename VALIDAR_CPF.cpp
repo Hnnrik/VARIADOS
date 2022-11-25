91d2624ac068cdf1e832ae85176d38e392deefbd
Hnnrik <113845766+Hnnrik@users.noreply.github.com>
2022-11-25 14:51:23 -0300

    Update VALIDAR_CPF.cpp

--- VALIDAR_CPF.cpp
+++ VALIDAR_CPF.cpp
@@ -1,46 +1,57 @@
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
-
 int main(){
 	setlocale(LC_ALL,"");
-	int cpf[11];
-	int i,v1=0,vf,e,valido=0;
-	do{
-		do{
-			printf("Informe os dígitos do seu CPF.\n");
-			for(i=0;i<11;i++){
-				scanf("%d",&cpf[i]);
-			}
-			printf("Seu CPF é igual a: ");
-			printf("%d%d%d.%d%d%d.%d%d%d-%d%d?\n",cpf[0],cpf[1],cpf[2],
-			cpf[3],cpf[4],cpf[5],cpf[6],cpf[7],cpf[8],cpf[9],cpf[10]);
-			printf("1-SIM 2-NÃO\n");
-			scanf("%d", &e);
-		}while(e==2);
-			e=10;
+	char cpfChar[100];
+	int cpfInt[11], i;
+	gets(cpfChar);
+	for(i=0;i<12;i++){
+		if(cpfChar[i]=='0'){
+			cpfInt[i]=0;
+		}else if(cpfChar[i]=='1'){
+			cpfInt[i]=1;
+		}else if(cpfChar[i]=='2'){
+			cpfInt[i]=2;
+		}else if(cpfChar[i]=='3'){
+			cpfInt[i]=3;
+		}else if(cpfChar[i]=='4'){
+			cpfInt[i]=4;
+		}else if(cpfChar[i]=='5'){
+			cpfInt[i]=5;
+		}else if(cpfChar[i]=='6'){
+			cpfInt[i]=6;
+		}else if(cpfChar[i]=='7'){
+			cpfInt[i]=7;
+		}else if(cpfChar[i]=='8'){
+			cpfInt[i]=8;
+		}else if(cpfChar[i]=='9'){
+			cpfInt[i]=9;
+		}
+	}
+		int v1=0,vf,e=10,valido=0;
 		for(i=0;i<9;i++){
-			v1+=(cpf[i]*e);
+			v1+=(cpfInt[i]*e);
 			e--;
 		}
 		vf=v1%11;
 		vf=11-vf;
-		if(vf==cpf[9] || vf==11){
+		if(vf==cpfInt[9] || vf==11){
 			valido++;
 		}
 			e=11;
 		for(i=0;i<10;i++){
-			v1+=(cpf[i]*e);
+			v1+=(cpfInt[i]*e);
 			e--;
 		}
 		vf=v1%11;
 		vf=11-vf;
-		if(vf==cpf[10] || vf==11){
+		if(vf==cpfInt[10] || vf==11){
 			valido++;
 		}
 		if(valido==2){
-			printf("CPF válido.");
+			printf("CPF vÃ¡lido.");
 		}else{
-			printf("CPF inválido.");
+			printf("CPF invÃ¡lido.");
 		}
 }
